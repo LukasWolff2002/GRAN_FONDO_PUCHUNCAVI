fde19d4156d9f3a018895aa872daf4778bed5cac
LukasWolff2002 <lwolff@miuandes.cl>
2024-12-03 10:26:26 -0300

    avance

--- app/controllers/gran_fondo_puchuncavi_controller.rb
+++ app/controllers/gran_fondo_puchuncavi_controller.rb
@@ -1,6 +1,14 @@
 class GranFondoPuchuncaviController < ApplicationController
 
     def home
+        @sponsors = [
+            'auspiciadores/powerade.png',
+            'auspiciadores/cocacola.png',
+            'auspiciadores/benedictino.png',
+            'auspiciadores/budweiser.png',
+            'auspiciadores/carozzi.png',
+            'auspiciadores/all4bikers.png'
+            ]
     end
 
     def guide
